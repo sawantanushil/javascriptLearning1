4f346edf1d815843990714099da78c43cd013d01
sawantanushil <113376359+sawantanushil@users.noreply.github.com>
2022-12-29 21:04:47 +0530

    This is a demo programming
    This is a demo programming

---
+++ demo.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+console.log("My name is Anushil Sawant");
+console.info("This is information");
+console.error("Opps failed to exucation");
+console.warn("this is warrning");
